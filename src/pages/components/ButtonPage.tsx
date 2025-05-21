4729742e2de0e13f7083c9a918954f9bc1cc4db0
mjd0530 <mjd0530@gmail.com>
2025-05-21 08:33:42 -0400

    Center and narrow Button page content with a max-width container

--- src/pages/components/ButtonPage.tsx
+++ src/pages/components/ButtonPage.tsx
@@ -3,6 +3,12 @@ import styled from '@emotion/styled';
 import ComponentPage from '../../components/ComponentPage/ComponentPage';
 import { Button } from '../../components/Button/Button';
 
+const Container = styled.div`
+  max-width: 700px;
+  margin: 0 auto;
+  width: 100%;
+`;
+
 const Section = styled.section`
   margin-bottom: 2rem;
 `;
@@ -25,31 +31,33 @@ import { Button } from '@/components/Button';
 `;
 
   return (
-    <ComponentPage
-      title="Button"
-      description="Buttons allow users to trigger an action or event with a single click. They communicate calls to action to your users and allow them to interact with your interface."
-      code={exampleCode}
-    >
-      <Section>
-        <ButtonGrid>
-          <div>
-            <Button>
-              Primary
-            </Button>
-          </div>
-          <div>
-            <Button variant="secondary">
-              Secondary
-            </Button>
-          </div>
-          <div>
-            <Button variant="danger">
-              Danger
-            </Button>
-          </div>
-        </ButtonGrid>
-      </Section>
-    </ComponentPage>
+    <Container>
+      <ComponentPage
+        title="Button"
+        description="Buttons allow users to trigger an action or event with a single click. They communicate calls to action to your users and allow them to interact with your interface."
+        code={exampleCode}
+      >
+        <Section>
+          <ButtonGrid>
+            <div>
+              <Button>
+                Primary
+              </Button>
+            </div>
+            <div>
+              <Button variant="secondary">
+                Secondary
+              </Button>
+            </div>
+            <div>
+              <Button variant="danger">
+                Danger
+              </Button>
+            </div>
+          </ButtonGrid>
+        </Section>
+      </ComponentPage>
+    </Container>
   );
 };
 
